7ca35d01dd29001cca15396ca90bbc1d809867c5
mieki256 <mieki256ld@gmail.com>
2017-03-19 18:02:58 +0900

    * add : sample image and sample result

---
+++ sample_test.bat
@@ -0,0 +1,3 @@
+python texpack.py -i sample_img\*.png -o sample_output\out0.png -x sample_output\out0.xml --show
+python texpack.py -i sample_img\*.png -o sample_output\out1.png -x sample_output\out1.xml --sortoff --mode nextfit --show
+python texpack.py -i sample_img\*.png -o sample_output\out2.png -x sample_output\out2.xml --trim --show
